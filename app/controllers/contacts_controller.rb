483b1e5b663a10c829eec392e17893b64d1a1657
rdrgzma <rdrgzma@hotmail.com>
2017-05-28 19:34:46 -0300

    add i18n

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,12 +5,12 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
 
-    render json: @contacts
+    render json: @contacts, methods: :birthdate_br
   end
 
   # GET /contacts/1
   def show
-    render json: @contact
+    render json: @contact, methods: :birthdate_br
   end
 
   # POST /contacts
@@ -27,7 +27,7 @@ class ContactsController < ApplicationController
   # PATCH/PUT /contacts/1
   def update
     if @contact.update(contact_params)
-      render json: @contact
+      render json: @contact, methods: :birthdate_br
     else
       render json: @contact.errors, status: :unprocessable_entity
     end
@@ -46,6 +46,6 @@ class ContactsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def contact_params
-      params.require(:contact).permit(:name, :email, :birthdate)
+      params.require(:contact).permit(:name, :email, :birthdate, :kind_id)
     end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,19 +1,23 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
+  belongs_to :kind, optional: true
 
-  def author
-    "Márcio Rodriguez"
+  def birthdate_br
+    I18n.l(self.birthdate) unless birthdate.blank?
   end
 
-  def kind_description
-    self.kind.description
-  end
-
-  def as_json(options={})
-    super(
-      root: true,
-      methods: [:kind_description, :author],
-      include: {kind: {only: :description}}
-      )
-  end
+  # def author
+  #   "Márcio Rodriguez"
+  # end
+  #
+  # def kind_description
+  #   self.kind.description
+  # end
+  #
+  # def as_json(options={})
+  #   super(
+  #     root: true,
+  #     methods: [:kind_description, :author],
+  #     include: {kind: {only: :description}}
+  #     )
+  # end
 end
